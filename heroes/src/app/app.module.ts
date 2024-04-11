4be0b5cf9b71a560b5a06f23fa0c5e51e2d47eb7
Ronald Rengifo <ronaldpptx@gmail.com>
2024-04-10 21:58:51 -0500

    commit inicial

---
+++ heroes/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import ( APP ROUTING from './app.routes';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BiografiaComponent } from './components/biografia/biografia.component';
+import { FooderComponent } from './components/fooder/fooder.component';
+import { HistoriaComponent } from './components/historia/historia.component';
+import { NavbarComponent} from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BiografiaComponent,
+    FooderComponent,
+    HistoriaComponent,
+    NavbarComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ heroes/src/app/app.routes.ts
@@ -0,0 +1,2 @@
+import { RouterModule,Routes } from "@angular/router";
+import { navbarNavAltMarkup } from './components/navbar/navbar.component';
